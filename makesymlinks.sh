f8ba094142c7b4d5393526cea8e8f648a1a6de4e
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2013-03-11 00:15:12 -0700

    better handling of existing links

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -15,19 +15,39 @@ files="bashrc bash_profile emacs screenrc sshconfig keydoublerc"    # list of fi
 ##########
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup ..."
 mkdir -p $olddir
-echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo -n "cd to $dir ..."
 cd $dir
 echo "done"
 
+
+function create_link {
+	echo "Creating symlink to $1"
+	ln -s $dir/$1 ~/.$1
+}
+
+function backup_file {
+	echo "Moving .$1 from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old/
+}
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+	if [ -e ~/.$file ]
+	then
+		if [ -L ~/.$file ]
+		then
+			echo "Skip existing symlink for ${file}"
+		else
+			backup_file ${file}
+			create_link $file
+		fi
+	else
+		create_link $file
+	fi
 done
+
+cd -
